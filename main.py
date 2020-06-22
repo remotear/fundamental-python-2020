6f6ca14d90b7ed49765bbf1d00ecdea9cef5e588
RJP <koekudukuad@gmail.com>
2020-06-23 01:54:17 +0700

    added new line

---
+++ main.py
@@ -0,0 +1,15 @@
+#KONTRUKSI DATA PYTHON
+# SEQUENTIAL
+print('hello word')
+print('by Arsanto')
+print('Tanggal 23 06 2020')
+print('_' * 10)
+
+
+#PERCABANGAN
+ingin_cepat = False
+if ingin_cepat:
+    print('Jalan lurus saya ya!')
+else:
+    print('Jalan memutar')
+
